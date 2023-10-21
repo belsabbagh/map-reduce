fee4c04334a1c2b80f85ad8deb98ca0a3db23dd2
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-10-21 19:45:01 +0300

    first test with word count

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-if __name__ == '__main__':
-    pass
+from src import Mapper, Reducer, Grouper, MapWithDuplicateKeys
+
+# word count example
+
+
+def map_fn(text):
+    _map = MapWithDuplicateKeys()
+    for word in text.split():
+        _map(**{word: 1})
+    return _map._map
+
+
+def group_fn(maps):
+    merged_map = []
+    for m in maps:
+        merged_map.extend(m)
+    keys = set([i[0] for i in merged_map])
+    grouped_map = []
+    for key in keys:
+        shard = []
+        for i in merged_map:
+            if i[0] == key:
+                shard.append(i)
+        grouped_map.append(shard)
+    return grouped_map
+
+
+def reduce_fn(m: list[tuple]):
+    return (m[0][0], sum([i[1] for i in m]))
+
+
+if __name__ == "__main__":
+    text = "hello world\nhey you\nhello world"
+    shards = text.split("\n")
+    maps = [Mapper(map_fn)(i) for i in shards]
+    print(maps)
+    new_maps = Grouper(group_fn)(maps)
+    print(new_maps)
+    reductions = [Reducer(reduce_fn)(i) for i in new_maps]
+    print(reductions)
---
+++ src/__init__.py
@@ -0,0 +1,46 @@
+class Mapper:
+    def __init__(self, map_fn) -> None:
+        self.map_fn = map_fn
+
+    def __call__(self, data, *args, **kwargs):
+        return self.map_fn(data, *args, **kwargs)
+
+
+class Reducer:
+    def __init__(self, reduce_fn) -> None:
+        self.reduce_fn = reduce_fn
+
+    def __call__(self, maps, *args, **kwargs):
+        return self.reduce_fn(maps, *args, **kwargs)
+
+
+class Shuffler:
+    def __init__(self, shuffle_fn) -> None:
+        self.shuffle_fn = shuffle_fn
+
+    def __call__(self, maps, *args, **kwargs):
+        return self.shuffle_fn(maps, *args, **kwargs)
+
+
+class Grouper(Shuffler):
+    def __init__(self, group_fn) -> None:
+        super().__init__(group_fn)
+
+
+class MapReduce:
+    def __init__(self, mapper, reducer) -> None:
+        self.mapper = mapper
+        self.reducer = reducer
+
+    def __call__(self, *args, **kwargs):
+        return self.reducer(self.mapper(*args, **kwargs))
+
+
+class MapWithDuplicateKeys:
+    _map: list[tuple]
+
+    def __init__(self) -> None:
+        self._map = []
+
+    def __call__(self, **kwargs):
+        self._map.extend(kwargs.items())
